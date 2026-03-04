e4809b15af65701eacbe18830e08d5ebc030a58e
栗子 <3471023785@qq.com>
2026-03-04 11:58:35 +0800

    Restore full website with GitHub Pages config

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from './views/Home.vue'
 import About from './views/About.vue'
 import Projects from './views/Projects.vue'
@@ -14,7 +14,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
